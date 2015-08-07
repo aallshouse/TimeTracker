f0682e1076dfc7d23dea5de5708f7834e159d4ab
Andrew Allshouse <aallshouse@matrixformedia.com>
2015-08-06 20:53:23 -0400

    Clean up css and positioning for editing a user's roles

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150515015537) do
+ActiveRecord::Schema.define(version: 20150529010032) do
 
   create_table "roles", force: :cascade do |t|
     t.string   "name",          limit: 255
