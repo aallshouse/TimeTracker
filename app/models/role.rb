632b5e6ff1da65e5a34327a2130811ac810e38ab
Andrew Allshouse <aallshouse@matrixformedia.com>
2015-08-06 21:53:27 -0400

    Tie roles to their work items through the join table roles_work_items

--- app/models/role.rb
+++ app/models/role.rb
@@ -14,7 +14,7 @@ class Role < ActiveRecord::Base
   has_and_belongs_to_many :users, :join_table => :users_roles
   belongs_to :resource, :polymorphic => true
 
-  has_many :work_items
+  has_and_belongs_to_many :work_items, :join_table => :roles_work_items
 
   scopify
 end
