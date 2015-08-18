52abb21bb6cf7bfe6d32d05a536a65ada31d0ba3
Andrew Allshouse <aallshouse@matrixformedia.com>
2015-08-17 21:43:57 -0400

    Update new migrations to remove duplicate migration calls

--- db/migrate/20150818012803_fixworkitems.rb
+++ db/migrate/20150818012803_fixworkitems.rb
@@ -1,6 +1,6 @@
 class Fixworkitems < ActiveRecord::Migration
   def change
-  	add_column :work_items, :work_item_type_id, :integer
+  	#add_column :work_items, :work_item_type_id, :integer
   	remove_column :work_items, :user_id, :integer
   end
 end
